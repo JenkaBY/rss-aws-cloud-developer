5165fbc4ae8c465fa0cf8be49606466178a4f5f2
evgen.kuzmich <evgen.kuzmich@gmail.com>
2024-06-30 00:13:15 +0300

    ui update

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -5,17 +5,17 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://6ka0pqc4k6.execute-api.eu-north-1.amazonaws.com/prod/db',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://zz96tej570.execute-api.eu-north-1.amazonaws.com/prod',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: false,
     cart: false,
   },
   accessibleUrl: "https://d1dkpi5wt9dvqq.cloudfront.net/",
-  taskIdentification: 'Task 4. GET and POST endpoints enabled. The data persisted in DynamoDb. Deployed via Java CDK.'
+  taskIdentification: 'Task 5. Turn the import-files-api on. Deployed via Java CDK.'
 };
