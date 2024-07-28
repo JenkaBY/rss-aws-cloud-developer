bfdf35131d43c9b678f690feccc6825e7578900e
evgen.kuzmich <evgen.kuzmich@gmail.com>
2024-07-28 23:07:38 +0300

    update UI with Cart API

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,21 +1,23 @@
-import {Config} from './config.interface';
+import { Config } from './config.interface';
 
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://wgti49plnh.execute-api.eu-north-1.amazonaws.com/prod/',
+    product: 'https://wgti49plnh.execute-api.eu-north-1.amazonaws.com/prod',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://zz96tej570.execute-api.eu-north-1.amazonaws.com/prod',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://fzpbwjly4d.execute-api.eu-north-1.amazonaws.com/prod',
+    auth: 'https://fzpbwjly4d.execute-api.eu-north-1.amazonaws.com/prod',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
     import: true,
     bff: false,
-    cart: false,
+    cart: true,
+    auth: true,
   },
   accessibleUrl: "https://d1hxiyo0kk0n80.cloudfront.net/",
-  taskIdentification: 'Task 7. Basic Authorizer. Deployed via Java CDK.'
+  taskIdentification: 'Task 8. Cart Service API. Postgres instance. Deployed via Java CDK.'
 };
