47124a758797d3ea927427afd6616e2d36030d63
evgen.kuzmich <evgen.kuzmich@gmail.com>
2024-08-11 19:39:45 +0300

    Update UI with BFF service url

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
-import { Config } from './config.interface';
+import {Config} from './config.interface';
 
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://wgti49plnh.execute-api.eu-north-1.amazonaws.com/prod',
+    product: 'http://jenkaby-bff-api-prod.eu-north-1.elasticbeanstalk.com/product/',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://zz96tej570.execute-api.eu-north-1.amazonaws.com/prod',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'http://jenkaby-cart-api-prod.eu-north-1.elasticbeanstalk.com',
-    auth: 'http://jenkaby-cart-api-prod.eu-north-1.elasticbeanstalk.com',
+    cart: 'http://jenkaby-bff-api-prod.eu-north-1.elasticbeanstalk.com/cart/',
+    auth: 'http://jenkaby-bff-api-prod.eu-north-1.elasticbeanstalk.com/cart/',
   },
   apiEndpointsEnabled: {
     product: true,
@@ -19,5 +19,5 @@ export const environment: Config = {
     auth: true,
   },
   accessibleUrl: "http://d1hxiyo0kk0n80.cloudfront.net/",
-  taskIdentification: 'Task 9. Docker and AWS Elastic Beanstalk'
+  taskIdentification: 'Task 10. Backend For Frontend'
 };
